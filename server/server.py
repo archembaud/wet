650e6a3a5fddec36d1c1aec15218a8787cca708c
Vince-2021 <minchungyi@gmail.com>
2021-06-12 19:53:33 +1000

    Add Server
    This is the first try at adding the server.

---
+++ server/server.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/hello/', methods=['GET', 'POST'])
+def welcome():
+    return "Hello World!"
+if __name__ == '__main__':
+    print("Starting the REST server")
+    app.run(host='0.0.0.0', port=105)
