b429494b15831cecd0fd2afad7a16d0ae58b3b8d
Matt Smith <mr.smith@fugro.com>
2021-06-12 20:00:41 +1000

    Add comments to server
    This change i) adds comments to the server file, and
    ii) limits the hello endpoint to GET only.

--- server/server.py
+++ server/server.py
@@ -1,8 +1,19 @@
 from flask import Flask
+
+# Create the server app
 app = Flask(__name__)
-@app.route('/hello/', methods=['GET', 'POST'])
+
+
+# Define what happens when the user performs
+# a GET request on the hello endpoint
+# Sample use
+# curl -X GET http://0.0.0.0:105/hello/
+@app.route('/hello/', methods=['GET'])
 def welcome():
     return "Hello World!"
+
+
+# Entry point
 if __name__ == '__main__':
     print("Starting the REST server")
     app.run(host='0.0.0.0', port=105)
